ba5ca877a5aea1576f21511408287c0236b3b67e
parthmodi23 <parthmodi22500@gmail.com>
2023-07-18 18:33:46 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-function App() {
+
+function MyApp() {
+  const [postcount, setPostcount] = useState([]);
+  const [selectedMovie, setSelectedMovie] = useState(null);
+
+  useEffect(() => {
+    axios
+      .get("https://www.omdbapi.com/?s=man&apikey=4a3b711b")
+      .then((res) => {
+        // Assuming the response data is an array of movies
+        setPostcount(res.data.Search);
+      })
+  }, []);
+
+  // Function to show movie details when the "Movie details" button is clicked
+  const showMovieDetails = (movie) => {
+    setSelectedMovie(movie);
+  };
+
+  // Function to close movie details and go back to the movie list
+  const closeMovieDetails = () => {
+    setSelectedMovie(null);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {/* Show the list of movies */}
+      {!selectedMovie && (
+        <><div className="grid">
+           {postcount.map((post) => {
+            const { imdbID, Title, Poster } = post;
+            return (
+              <div className="card" style={{ width: "18rem" }} key={imdbID}>
+                <img src={Poster} className="card-img-top" alt="..." />
+                <div className="card-body">
+                  <h1>{Title}</h1>
+                  <button className="btn btn-primary"
+                    onClick={() => showMovieDetails(post)}>Movie deta</button>
+                </div>
+              </div>
+            );
+          })}
+          </div>
+        </>
+      )}
+
+      {/* Show movie details if a movie is selected */}
+      {selectedMovie && (
+        <div ClassName="card" style={{ width: "18rem" }}>
+          <h1>Movie Name:-{selectedMovie.Title}</h1>
+          <h2>Release Year:-{selectedMovie.Year}</h2>
+          <h3>Type:-{selectedMovie.Type}</h3>
+          <img src={selectedMovie.Poster} alt="Movie Poster" />
+          <button className="btn btn-primary" onClick={closeMovieDetails}> Close Movie Details </button>
+        </div>
+      )}
+    </>
   );
 }
 
-export default App;
+export default MyApp;
